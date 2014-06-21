ca843c221885ff27e9b6954189bba63715052afb
swhgoon <username@users.noreply.github.com>
2014-06-21 23:12:12 +0800

    part1_devdataprod-shiny

--- README.md
+++ README.md
@@ -1,36 +1,43 @@
-# Course Project
-@https://www.coursera.org/course/regmods
+# Course Project  
+@https://www.coursera.org/course/regmods  
 @https://www.coursera.org/course/devdataprod
 
 ____
-## Part0: Regression Models Course Project
-> **Context**
-
-> You work for Motor Trend, a magazine about the automobile industry. Looking at a data set of a collection of cars, they are interested in exploring the relationship between a set of variables and miles per gallon (MPG) (outcome). They are particularly interested in the following two questions:
+## Part0: Regression Models Course Project  
+> **Context**  
+You work for Motor Trend, a magazine about the automobile industry. Looking at a data set of a collection of cars, they are interested in exploring the relationship between a set of variables and miles per gallon (MPG) (outcome). They are particularly interested in the following two questions:
 > * “Is an automatic or manual transmission better for MPG”
 > * “Quantifying how different is the MPG between automatic and manual transmissions?”
 
-> **Question**
+> **Question**  
+Take the `mtcars` data set and write up an analysis to answer their question using regression models and exploratory data analyses.
 
-> Take the mtcars data set and write up an analysis to answer their question using regression models and exploratory data analyses.
+> **Your report must be:**  
+* Written as a PDF printout of a compiled (using knitr) R markdown document.
+ * Do not use any packages that are not in R-base or the library datasets.
+* Brief. Roughly the equivalent of 2 pages or less for the main text. Supporting figures in an appendix can be included up to 5 total pages including the 2 for the main report. The appendix can only include figures.
+* Include a first paragraph executive summary.
 
-> Your report must be:
+### Executive Summary: 
+There is a set of variables related to miles per gallon (MPG) (outcome).
 
-> * Written as a PDF printout of a compiled (using knitr) R markdown document.
->  * Do not use any packages that are not in R-base or the library datasets.
-> * Brief. Roughly the equivalent of 2 pages or less for the main text. Supporting figures in an appendix can be included up to 5 total pages including the 2 for the main report. The appendix can only include figures.
-> * Include a first paragraph executive summary.
+Here the difference of the MPG between automatic and manual transmissions is focused, 
+the `mtcars` dataset in car package is taken as the data source, 
+and techniques about regression models is used to solve the following two questions:
 
-0
+* “Is an automatic or manual transmission better for MPG”
+*  “Quantifying how different is the MPG between automatic and manual transmissions?”
+
 
 ____
-## Part1: Shiny Application
-> 1. Write a shiny application with associated supporting documentation. The documentation should be thought of as whatever a user will need to get started using your application.
+## Part1: Shiny Application  
+> 
+1. Write a shiny application with associated supporting documentation. The documentation should be thought of as whatever a user will need to get started using your application.
 2. Deploy the application on Rstudio's shiny server
 3. Share the application link by pasting it into the text box below
 4. Share your server.R and ui.R code on github
 
-> **The application must include the following:**
+> **The application must include the following:**  
 1. Some form of input (widget: textbox, radio button, checkbox, ...)
 2. Some operation on the ui input in sever.R
 3. Some reactive output displayed as a result of server calculations
@@ -40,15 +47,15 @@ ____
 1
 
 ____
-##Part2: Reproducible Pitch Presentation
+##Part2: Reproducible Pitch Presentation  
 > OK, you've made your shiny app, now it's time to make your pitch. You get 5 slides (inclusive of the title slide)  to pitch a your app. You're going to create a web page using Slidify with an html5 slide deck.
 
-> **Here's what you need**
+> **Here's what you need**  
 * 5 slides to pitch our idea done in Slidify
 * Your presentation pushed to github
 * A link to your github presentation pasted into the text box below
 
-> **Your presentation must satisfy the following**
+> **Your presentation must satisfy the following**  
 * It must be done in Slidify
 * It must be 5 pages
 * It must be on github
--- part1_devdataprod-shiny/ui.R
+++ part1_devdataprod-shiny/ui.R
@@ -1,39 +1,71 @@
 library(shiny)
 
-shinyUI(fluidPage(
-  
-  titlePanel("The relationship between other variables and miles per gallon (MPG)"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("variable", "Variable:",
-                  c("Number of cylinders" = "cyl",
-                    "Displacement (cu.in.)" = "disp",
-                    "Gross horsepower" = "hp",
-                    "Rear axle ratio" = "drat",
-                    "Weight (lb/1000)" = "wt",
-                    "1/4 mile time" = "qsec",
-                    "V/S" = "vs",
-                    "Transmission" = "am",
-                    "Number of forward gears" = "gear",
-                    "Number of carburetors" = "carb"
-                    )),
-  
-      checkboxInput("outliers", "Show BoxPlot's outliers", FALSE)
-    ),
-    
-    mainPanel(
-      h3(textOutput("caption")),
-      
-      tabsetPanel(type = "tabs", 
-                  tabPanel("BoxPlot", plotOutput("mpgBoxPlot")),
-                  tabPanel("Regression model", 
-                           plotOutput("mpgPlot"),
-                           verbatimTextOutput("fit")
-                           )
-                  )
-      ),
-    
-    )
+shinyUI(
+  navbarPage("mtcars dataset",
+             tabPanel("Detail",
+                      h2("Motor Trend Car Road Tests"),
+                      hr(),
+                      h3("Description"),
+                      helpText("The data was extracted from the 1974 Motor Trend US magazine,",
+                               " and comprises fuel consumption and 10 aspects of automobile design and performance",
+                               " for 32 automobiles (1973–74 models)."),
+                      h3("Format"),
+                      p("A data frame with 32 observations on 11 variables."),
+                        
+                      p("  [, 1]   mpg	 Miles/(US) gallon"),
+                      p("  [, 2]	 cyl	 Number of cylinders"),
+                      p("  [, 3]	 disp	 Displacement (cu.in.)"),
+                      p("  [, 4]	 hp	 Gross horsepower"),
+                      p("  [, 5]	 drat	 Rear axle ratio"),
+                      p("  [, 6]	 wt	 Weight (lb/1000)"),
+                      p("  [, 7]	 qsec	 1/4 mile time"),
+                      p("  [, 8]	 vs	 V/S"),
+                      p("  [, 9]	 am	 Transmission (0 = automatic, 1 = manual)"),
+                      p("  [,10]	 gear	 Number of forward gears"),
+                      p("  [,11]	 carb	 Number of carburetors"),
+                      
+                      h3("Source"),
+                      
+                      p("Henderson and Velleman (1981), Building multiple regression models interactively. Biometrics, 37, 391–411.")
+             ),
+             tabPanel("Analysis",
+                      fluidPage(
+                        titlePanel("The relationship between other variables and miles per gallon (MPG)"),
+                        sidebarLayout(
+                          sidebarPanel(
+                            selectInput("variable", "Variable:",
+                                        c("Number of cylinders" = "cyl",
+                                          "Displacement (cu.in.)" = "disp",
+                                          "Gross horsepower" = "hp",
+                                          "Rear axle ratio" = "drat",
+                                          "Weight (lb/1000)" = "wt",
+                                          "1/4 mile time" = "qsec",
+                                          "V/S" = "vs",
+                                          "Transmission" = "am",
+                                          "Number of forward gears" = "gear",
+                                          "Number of carburetors" = "carb"
+                                        )),
+                            
+                            checkboxInput("outliers", "Show BoxPlot's outliers", FALSE)
+                          ),
+                          
+                          mainPanel(
+                            h3(textOutput("caption")),
+                            
+                            tabsetPanel(type = "tabs", 
+                                        tabPanel("BoxPlot", plotOutput("mpgBoxPlot")),
+                                        tabPanel("Regression model", 
+                                                 plotOutput("mpgPlot"),
+                                                 verbatimTextOutput("fit")
+                                        )
+                            )
+                          )
+                        )
+                      )
+             ),
+             tabPanel("SourceCode",
+                      p("part1_devdataprod-shiny"),
+                      a("https://github.com/swhgoon/devdataprod-cp/tree/master/part1_devdataprod-shiny")
+                      )
   )
 )
